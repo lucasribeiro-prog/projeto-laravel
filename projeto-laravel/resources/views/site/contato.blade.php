36a6623d0e6aa84b5c090931249c6c77cb770771
Lucas Ribeiro <lucasribeiro1999.ll@gmail.com>
2024-09-04 21:46:50 -0300

    Criando um arquivo separado para armazenar o fomulario e adicionando ele nas views atraves de um @component

--- projeto-laravel/resources/views/site/contato.blade.php
+++ projeto-laravel/resources/views/site/contato.blade.php
@@ -8,27 +8,10 @@
         </div>
 
         <div class="informacao-pagina">
-            <div class="contato-principal">
-                <form action="{{ route('site.contato') }}" method="POST">
-                    @csrf
-                    <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="1">Dúvida</option>
-                        <option value="2">Elogio</option>
-                        <option value="3">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
-            </div>
+            @component('site.layouts._components.form', ['class' => 'borda-preta'])
+                <p>Sua solicitação será atendida em breve</p>
+                <p>O tempo médio de retorno é de 24h</p>
+            @endcomponent
         </div>  
     </div>
 
