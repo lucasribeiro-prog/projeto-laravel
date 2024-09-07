d1cc30d8015c63ccf382c368bf9c51c036b8c85f
Lucas Ribeiro <lucasribeiro1999.ll@gmail.com>
2024-09-07 14:38:01 -0300

    Criando a tabela unidades e estabelecendo a relação de um para muitos com as tabelas produtos e produto_detalhes

---
+++ projeto-laravel/database/migrations/2024_09_07_160348_create_unidades_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 50);
+            $table->timestamps();
+        });
+
+        //Adicionando o relacionamento com a tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        //Adicionando o relacionamento com a tabela produtos
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Removendo o relacionamento com a tabela produto_detalhes
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->dropForeign('unidade_id');
+            $table->dropColumn('unidade_id');
+        });
+
+        //Removendo o relacionamento com a tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('unidade_id');
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::dropIfExists('unidades');
+    }
+};
