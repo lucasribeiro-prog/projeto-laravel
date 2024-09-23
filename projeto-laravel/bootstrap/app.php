7b0a8dd698974fa298a23f667dc368beea6d2220
Lucas Ribeiro <lucasribeiro1999.ll@gmail.com>
2024-09-22 21:15:11 -0300

    Criando uma nova tabela no banco de dados e implementando um middleware para todas as rotas

--- projeto-laravel/bootstrap/app.php
+++ projeto-laravel/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(LogAcessoMiddleware::class);
     })->create();
